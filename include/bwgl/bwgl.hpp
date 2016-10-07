ed9a0a95c83697a33ff7e6a01ea51283244da1fe
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-10-07 11:19:20 -0700

    Set up bwgl.hpp as only external include header

---
+++ include/bwgl/bwgl.hpp
@@ -0,0 +1,13 @@
+/*
+ * BWGL - Bro, Where's ur Graphics Library?
+ * Author: Benjamin Wiberg
+ *
+ * A simple OpenGL C++ utility toolkit. It provides wrapper classes
+ * for OpenGL concepts such as VBOs, VAOs, framebuffers, textures etc.
+ *
+ * License: "WTFPL" http://www.wtfpl.net/txt/copying/
+ * */
+
+#pragma once
+
+#include <VertexBufferObject.hpp>
